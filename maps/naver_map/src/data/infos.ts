436b30a417b64da29074c3cf1749ec5bca037a6f
seunghyeon <ksh990913@gmail.com>
2023-05-03 19:51:03 +0900

    Feat: 더미데이터 추가

---
+++ maps/naver_map/src/data/infos.ts
@@ -0,0 +1,92 @@
+export const infos = [
+  {
+    id: 1,
+    addressName: "서울 용산구 동자동 43-205",
+    placeName: "서울역",
+    position: {
+      lat: 37.5546788388674,
+      lng: 126.970606917394,
+    },
+  },
+  {
+    id: 2,
+    addressName: "서울 강남구 역삼동 858",
+    placeName: "강남역 2호선",
+    position: {
+      lat: 37.49808633653005,
+      lng: 127.02800140627488,
+    },
+  },
+  {
+    id: 3,
+    addressName: "서울 관악구 봉천동 979-2",
+    placeName: "서울대입구역 2호선",
+    position: {
+      lat: 37.4812845080678,
+      lng: 126.952713197762,
+    },
+  },
+  {
+    id: 4,
+    addressName: "서울 서대문구 연희동 5-1552",
+    placeName: "연희몰",
+    position: {
+      lat: 37.568018746876446,
+      lng: 126.92264614472266,
+    },
+  },
+  {
+    id: 5,
+    addressName: "서울 강서구 화곡동 980-1",
+    placeName: "화곡역",
+    position: {
+      lat: 37.541491121733786,
+      lng: 126.84013726441437,
+    },
+  },
+  {
+    id: 6,
+    addressName: "서울 중구 신당동 301-4",
+    placeName: "청계천",
+    position: {
+      lat: 37.56629586077714,
+      lng: 127.01119227657263,
+    },
+  },
+  {
+    id: 7,
+    addressName: "서울 중랑구 망우동 546-3",
+    placeName: "망우역",
+    position: {
+      lat: 37.599760222158745,
+      lng: 127.09193259756698,
+    },
+  },
+  {
+    id: 8,
+    addressName: "서울 강북구 미아동 916-5",
+    placeName: "미아사거리역",
+    position: {
+      lat: 37.613706938924424,
+      lng: 127.03095794420011,
+    },
+  },
+  {
+    id: 9,
+    addressName: "서울 송파구 문정동 619-1",
+    placeName: "문정역",
+    position: {
+      lat: 37.48516652608696,
+      lng: 127.12227629114415,
+    },
+  },
+  {
+    id: 10,
+    addressName: "서울 성동구 성수동1가 656-449",
+    placeName: "성수역",
+    position: {
+      lat: 37.54441521752088,
+      lng: 127.05604069376573,
+    },
+  },
+];
